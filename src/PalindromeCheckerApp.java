8301bf06b9d82a4ad501f3714f31e7c8efe27033
Samarth230 <samarthalambain@gmail.com>
2026-02-19 10:30:40 +0530

    added the hard palindrome system

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,9 @@
-public class PalindromeCheckerApp {
-
-    /* Use Case 1: Application Entry and Welcome Message
+/* Use Case 1: Application Entry and Welcome Message
     Description:
     This class represents the entry point of the Palindrome Checker Management System
     */
 
+public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker Management System");
         System.out.println("Version 1.0");
@@ -13,10 +12,21 @@ public class PalindromeCheckerApp {
         /* Use Case 2: Hard Coded Palindrome Validation System
          */
 
-        for(i=0;i<input.length()/2;i++){
-            
+    String input = "madam";
+    boolean palindrome = true;
+    char Arr[]=input.toLowerCase().toCharArray();0
+    for(int i=0;i<input.length()/2;i++){
+       if(input.charAt(i)!=input.charAt(input.length()-1-i)){
+           palindrome = false;
+           break;
         }
     }
+    if(palindrome)
+        System.out.println("It is a palindrome");
+    else
+        System.out.println("It is not a palindrome");
+    }
+
 }
 
 
